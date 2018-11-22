95fabe35ceb372dbb3fbc0e4cff56e1d4016817e
pulinau <pulinau@gmail.com>
2018-11-22 18:20:11 +0530

    Remove redundant code

--- src/app/services/assessment.service.ts
+++ src/app/services/assessment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Forest, Timber, Logs, TransmissionPoles, FencePosts, RoundPoles } from "../core/models/forest";
+import { Forest, Tree, Logs, TransmissionPoles, FencePosts, RoundPoles } from "../core/models/forest";
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentSnapshot } from "@angular/fire/firestore";
 import { Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -17,14 +17,6 @@ export class AssessmentService {
   }
 
   addForest(forest) {
-    // // const forestsCollection: AngularFirestoreCollection<Forest> = this.afs.collection("forests");
-    // const data: Forest = {
-    //   division: forest.division,
-    //   beat: forest.beat,
-    //   range: forest.range,
-    //   block: forest.block,
-    //   sBlock: forest.sBlock
-    // }
     return this.forestsCollection.add(forest);
   }
 
