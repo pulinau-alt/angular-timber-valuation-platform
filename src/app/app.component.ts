0d8e90f438cff8cd8dd9d08a3ab3c46d021f007b
pulinau <pulinau@gmail.com>
2019-01-02 21:34:47 +0530

    Add hide sidebar when not logged in function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './core/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-timber-valuation-platform';
+
+  constructor(public auth: AuthService) {}
 }
