e3d8a75a12781a7da36fc44a8d26ba4445901463
pulinau <pulinau@gmail.com>
2019-01-03 12:07:49 +0530

    Add login form and Google authentication

--- src/app/login-register/login-register.module.ts
+++ src/app/login-register/login-register.module.ts
@@ -1,11 +1,17 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent],
+  exports: [LoginComponent]
 })
 export class LoginRegisterModule { }
--- src/app/login-register/login/login.component.ts
+++ src/app/login-register/login/login.component.ts
@@ -1,3 +1,5 @@
+import { AuthService } from './../../core/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private auth: AuthService) {
+    this.initForm();
+  }
+
+  private googleLogin() {
+    this.auth.googleLogin();
+  }
 
   ngOnInit() {
+  }
+
+  private initForm() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
 }
